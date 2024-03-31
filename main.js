bf7da9f24f47a10243b27398bd6c51dbb3d4199c
MubashirKhanYousufZai <161746933+MubashirKhanYousufZai@users.noreply.github.com>
2024-03-31 22:48:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+// BASIC FUNCTIONS:
+function greet() {
+    return "HELLO WORLD";
+}
+var message = greet();
+console.log(message);
+function add() {
+    return 10000 + 1;
+}
+var ansOfAdd = add();
+console.log(ansOfAdd);
---
+++ main.ts
@@ -0,0 +1,15 @@
+// BASIC FUNCTIONS:
+function greet() {
+    return "HELLO WORLD"
+}
+let message=greet()
+console.log(message);
+ 
+
+function add() {
+    return 10000 + 1
+}
+
+let ansOfAdd=add()
+console.log(ansOfAdd);
+
